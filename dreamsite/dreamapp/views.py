556766dc641c43b2c01c6db6b8e02c4fd4afa04f
Sagar Shinde <asagarshinde@gmail.com>
2017-12-05 17:47:26 +0530

    Add files via upload

--- dreamsite/dreamapp/views.py
+++ dreamsite/dreamapp/views.py
@@ -4,3 +4,7 @@ from django.template import loader
 def index(request):
     template = loader.get_template('dreamapp/index.html')
     return HttpResponse(template.render())
+
+def linux(request):
+    template = loader.get_template('dreamapp/linux.html')
+    return HttpResponse(template.render())
